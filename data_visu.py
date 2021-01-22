da3157f3209bc5772afed35c94e6d99242917daa
aryamangodara <77824849+aryamangodara@users.noreply.github.com>
2021-01-22 15:57:24 +0530

    Delete data_visu.py

--- data_visu.py
+++
@@ -1,9 +0,0 @@
-#import numpy as np
-import matplotlib.pyplot as plt
-x= [i for i in range (-10,11)]
-y= [i**2 for i in range (-10,11)]
-act=["something","went","wrong"]
-time=[6,7,11]
-exp=[0.1,0.1,0.1]
-plt.pie(time,labels=act,shadow=True,explode=exp)
-plt.show()
